51b121fafca2c16d445ad93e48b315aea5fedc77
reito <cnschwarzer@qq.com>
2023-01-20 20:15:09 +0800

    initial

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# i18npp
+
+A simple C++ localization library.
+
---
+++ i18npp.h
@@ -0,0 +1,12 @@
+#ifndef I18NPP_I18NPP_H
+#define I18NPP_I18NPP_H
+
+namespace i18npp {
+    class translator {
+
+    };
+
+    inline static translator tr;
+}
+
+#endif //I18NPP_I18NPP_H
